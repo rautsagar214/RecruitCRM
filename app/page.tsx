ccbac5b67ca6c17512a337c74c4610034fe7eb03
rautsagar214 <rautsagar214@gmail.com>
2025-04-09 14:33:52 +0530

    day3 error solving

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
 'use client';
-
-
-
-import { useState } from "react";
-
 export default function Home() {
 
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+ 
 
   return (
-    <div className="h-screen w-full flex">
+    <div >
     
     </div>
   );
